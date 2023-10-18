180f42e8520682a318927ba9a910df3aa5e4ca4c
frankfullstack <frankfullstack@gmail.com>
2023-10-18 10:00:26 +0200

    Initialization

---
+++ apps/testing-session-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testing-session-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'testing-session-app';
+}
