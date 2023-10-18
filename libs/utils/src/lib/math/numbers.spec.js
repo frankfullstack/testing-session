42d7d6aa56d6d48897f267b0acedbff3e7705e1b
frankfullstack <frankfullstack@gmail.com>
2023-10-18 13:31:21 +0200

    Math Numbers utils tests file and some refactors

---
+++ libs/utils/src/lib/math/numbers.spec.js
@@ -0,0 +1,20 @@
+import { transformStringToNumber } from "./numbers";
+
+it('Should transform a string value to an equivalent type number', () => {
+    const input = '1';
+
+    const result = transformStringToNumber(input);
+
+    expect(result).toBe(1);
+    expect(result).toEqual(expect.any(Number))
+    // Or
+    expect(typeof result).toBe('number')
+})
+
+it('Should yield NaN for non-transformable values', () => {
+    const input = 'invalid';
+
+    const result = transformStringToNumber(input);
+
+    expect(result).toBeNaN();
+})
