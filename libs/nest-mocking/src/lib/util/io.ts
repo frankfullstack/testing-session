68c0d23b38e3855bee029f6d01b369c6641ef368
Francisco Jose Reyes Peralta <francisco.reyes@healios.io>
2023-10-19 13:55:01 +0200

    Added nest-mocking path mocking with latest argument and some other tests

--- libs/nest-mocking/src/lib/util/io.ts
+++ libs/nest-mocking/src/lib/util/io.ts
@@ -4,6 +4,6 @@ import { promises as fs } from 'fs';
 // Step 1: Recognize the side effect of writing a file in the File System
 // Step 3: Mocks para fs y path
 export default function writeData(data: string, filename: string) {
-    const storagePath = path.join(process.cwd(), 'data', filename);
+    const storagePath = path.join(process.cwd(), 'data', filename); // Step 4: remove to check if the path mock is working
     return fs.writeFile(storagePath, data);
 }
